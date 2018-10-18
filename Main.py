ee45e9907e2b31aedc38914ea79fa6882a13b8d3
MrTheGood <maarten@degoedeweb.nl>
2018-10-18 10:39:12 +0200

    Remove all code so far, set up a better base

--- Main.py
+++ Main.py
@@ -1,33 +1,41 @@
+#    Copyright 2018 Maarten de Goede
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 import pygame
 
+SIZE = SCREENWIDTH, SCREENHEIGHT = 600, 400
+BACKGROUND_COLOR = pygame.Color('white')
+FPS = 4
 
-def draw_board():
-    return
+
+def main():
+    global SCREEN, FPS_CLOCK
+    pygame.init()
+    FPS_CLOCK = pygame.time.Clock()
+    SCREEN = pygame.display.set_mode(SIZE)
+    done = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+                pygame.quit()
+                quit()
+
+        SCREEN.fill(BACKGROUND_COLOR)
+        pygame.display.update()
+        FPS_CLOCK.tick(FPS)
 
 
-COLUMN_COUNT = 7
-ROW_COUNT = 6
-CIRCLE_SIZE = 50
-CIRCLE_MARGIN = 20
-CIRCLE_COLOUR = (255, 255, 255)
-POS_MULTIPLIER = CIRCLE_SIZE + CIRCLE_MARGIN
-
-pygame.init()
-screen = pygame.display.set_mode((POS_MULTIPLIER * COLUMN_COUNT, POS_MULTIPLIER * ROW_COUNT))
-screen.fill((128, 0, 0))
-done = False
-
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-    pygame.display.flip()
-
-    for x in range(0, COLUMN_COUNT):
-        for y in range(0, ROW_COUNT):
-            pygame.draw.circle(screen, CIRCLE_COLOUR,
-                               (int(POS_MULTIPLIER * x + POS_MULTIPLIER / 2),
-                                int(POS_MULTIPLIER * y + POS_MULTIPLIER / 2)),
-                               int(CIRCLE_SIZE / 2)
-                               )
+if __name__ == '__main__':
+    main()
